9eb5e2c91495df4a9e63cdcca5121bc1a5472cfd
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2017-04-25 23:07:08 +0300

    Implemented js and css subcommands.

--- command.php
+++ command.php
@@ -17,29 +17,56 @@ class Assets_Pack_Command extends WP_CLI_Command {
      */
     protected $admin;
     
+    /**
+     * Creates a command.
+     */
     public function __construct() {
         parent::__construct();
         $this->admin = Assets_Pack_Admin::get_instance();
     }
     
     /**
-     * Show info.
+     * @param boolean $value
+     * @return string
+     */
+    protected function str_enabled( $value ) {
+        return $value ? 'enabled' : 'disabled';
+    }
+
+    /**
+     * @param array $value
+     * @return string
+     */
+    protected function str_skip( $value ) {
+        return count( $value ) ? implode( ', ', $value) : '-';
+    }
+    
+    /**
+     * Show JS status.
+     */
+    protected function info_js() {
+        WP_CLI::log( 'JS aggregation: ' . $this->str_enabled( $this->admin->get_setting( 'enable_js' ) ) );
+        WP_CLI::log( 'JS debug: ' . $this->str_enabled( $this->admin->get_setting( 'debug_js' ) ) );
+        WP_CLI::log( 'JS skip: ' . $this->str_skip( $this->admin->get_setting( 'skip_js' ) ) );
+    }
+    
+    /**
+     * Show CSS status.
+     */
+    protected function info_css() {
+        WP_CLI::log( 'CSS aggregation: ' . $this->str_enabled( $this->admin->get_setting( 'enable_css' ) ) );
+        WP_CLI::log( 'CSS debug: ' . $this->str_enabled( $this->admin->get_setting( 'debug_css' ) ) );
+        WP_CLI::log( 'CSS skip: ' . $this->str_skip( $this->admin->get_setting( 'skip_css' ) ) );
+    }
+    
+    /**
+     * Show plugin info.
      */
     public function info() {
-        $enabled = function ( $value ) {
-            return $value ? 'enabled' : 'disabled';
-        };
-        $skip = function ( $value ) {
-            return count( $value ) ? implode( ', ', $value ) : '-';
-        };
         WP_CLI::log( 'Assets dir: ' . $this->admin->get_setting( 'assets_dir' ) );
         WP_CLI::log( 'Assets url: ' . $this->admin->get_setting( 'assets_url' ) );
-        WP_CLI::log( 'JS aggregation: ' . $enabled( $this->admin->get_setting( 'enable_js' ) ) );
-        WP_CLI::log( 'JS debug: ' . $enabled( $this->admin->get_setting( 'debug_js' ) ) );
-        WP_CLI::log( 'JS skip: ' . $skip( $this->admin->get_setting( 'skip_js' ) ) );
-        WP_CLI::log( 'CSS aggregation: ' . $enabled( $this->admin->get_setting( 'enable_css' ) ) );
-        WP_CLI::log( 'CSS debug: ' . $enabled( $this->admin->get_setting( 'debug_css' ) ) );
-        WP_CLI::log( 'CSS skip: ' . $skip( $this->admin->get_setting( 'skip_css' ) ) );
+        $this->info_js();
+        $this->info_css();
     }
     
     /**
@@ -55,14 +82,85 @@ class Assets_Pack_Command extends WP_CLI_Command {
     /**
      * Scripts aggregation.
      *
+     * Show status or change scripts aggregation.
+     *
      * [--enable=<on|off>]
      * : Enable of disable scripts aggregation.
      * 
-     * [--debug=<on|off>]
+     * [--debug-info=<on|off>]
      * : Enable or disable debug mode.
+     * 
+     * [--skip=<scripts>]
+     * : Skip following scripts on aggregation. Where <scripts> is a list of script names
+     * separated by commas. To clear skip list pass empty string: --skip=""
      */
-    public function js() {
+    public function js( $args, $assoc_args ) {
+
+        $this->change_option( 'enable_js', 'enable', $assoc_args );
+        $this->change_option( 'debug_js', 'debug-info', $assoc_args );
+        $this->change_option( 'skip_js', 'skip', $assoc_args );
+
+        $this->info_js();
+    }
+
+    /**
+     * CSS styles aggregation.
+     *
+     * Show status or change styles aggregation.
+     *
+     * [--enable=<on|off>]
+     * : Enable of disable styles aggregation.
+     * 
+     * [--debug-info=<on|off>]
+     * : Enable or disable debug mode.
+     * 
+     * [--skip=<styles>]
+     * : Skip following styles on aggregation. Where <styles> is a list of style names
+     * separated by commas. To clear skip list pass empty string: --skip=""
+     */
+    public function css( $args, $assoc_args ) {
+
+        $this->change_option( 'enable_css', 'enable', $assoc_args );
+        $this->change_option( 'debug_css', 'debug-info', $assoc_args );
+        $this->change_option( 'skip_css', 'skip', $assoc_args );
+
+        $this->info_css();
+    }
+
+    /**
+     * Change plugin option.
+     *
+     * @param string $option Setting option name.
+     * @param string $cmd_opt Appropriate command line option name.
+     * @param array $args Command line arguments.
+     */
+    protected function change_option( $option, $cmd_opt, $args ) {
+        if ( !isset( $args[$cmd_opt] ) ) {
+            return;
+        }
         
+        $status = true;
+        $value = $args[$cmd_opt];
+        
+        switch ( $cmd_opt ) {
+            case 'enable':
+            case 'debug-info':
+                if ( $value === 'on' || $value === 'off' ) {
+                    $value = $value == 'on' ? true : false;
+                    $status = $this->admin->set_setting( $option, $value );
+                } else {
+                    WP_CLI::error( 'Options "enable", "debug" accept only "on" or "off".' );
+                }
+                break;
+                
+            case 'skip':
+                $status = $this->admin->set_setting( $option, $value );
+                break;
+        }
+        
+        if ( is_string( $status ) ) {
+            WP_CLI::error( $status );
+        }
     }
 }
 
